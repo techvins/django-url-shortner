a8a64db7bfd2d303a439a32fe76bc52799616b53
ajayrajpurohit111 <ajay@techvins.com>
2021-09-16 13:45:56 +0530

    When a URL is sent second time, it should return the previous short url

--- code/redirector/views.py
+++ code/redirector/views.py
@@ -2,6 +2,9 @@ from rest_framework import authentication, generics, permissions
 
 from .models import URLRedirect
 from .serializers import URLRedirectSerializer
+from rest_framework.response import Response
+from rest_framework import status
+
 
 
 class CreateRedirectorView(generics.CreateAPIView):
@@ -9,3 +12,14 @@ class CreateRedirectorView(generics.CreateAPIView):
     authentication_classes = [authentication.TokenAuthentication]
     queryset = URLRedirect.objects.none()
     serializer_class = URLRedirectSerializer
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=False)
+        url_redirect=URLRedirect.objects.filter(url=request.data['url'])
+        if url_redirect.exists():
+            short_url=url_redirect.last().short_url
+            return Response({'message':'already created','short_url':short_url}, status=status.HTTP_202_ACCEPTED)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
