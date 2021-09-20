c07aebb476f04776bebed07f85755a45388db8aa
ajayrajpurohit111 <ajay@techvins.com>
2021-09-20 20:18:50 +0530

    env file setup

--- code/dus/settings.py
+++ code/dus/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -73,15 +73,20 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'dus.wsgi.application'
 
-BASE_URL = ''
+BASE_URL =os.environ['BASE_URL']
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
+        
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD':os.environ['DB_PASSWORD'],
+        'HOST':os.environ['DB_HOST'],
+        'PORT':'3306',
     }
 }
 
@@ -104,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+DOMAIN_NAME=os.environ['DOMAIN_NAME']
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
