aba3652c56c7875cef35e40f9bc7a359031ab063
Vinay Techvins <vinay@techvins.com>
2021-09-21 06:04:28 +0530

    Default databse is now mysql

--- code/dus/settings.py
+++ code/dus/settings.py
@@ -81,7 +81,7 @@ BASE_URL =os.environ['BASE_URL']
 DATABASES = {
     'default': {
         
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': os.environ['DB_NAME'],
         'USER': os.environ['DB_USER'],
         'PASSWORD':os.environ['DB_PASSWORD'],
