847797d964d0cb718a9f8a68a4b65a6d269a8213
ajayrajpurohit111 <ajay@techvins.com>
2021-09-24 18:23:58 +0530

    admin options

--- code/redirector/admin.py
+++ code/redirector/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
 from .models import URLRedirect
 # Register your models here.
-admin.site.register(URLRedirect)
+
+
+class URLRedirectAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at','id','created_by')
+    fields = ['created_at','id','created_by','short_url','url','hit_count']
+    date_hierarchy = 'created_at'
+    list_display = ['id', 'short_url','url','hit_count','created_by']
+    sortable_by=['id', 'short_url','created_at']
+    ordering = ['id']
+    list_filter = ['created_at']
+    search_fields=['url','short_url','id']
+    list_display_links=['id','short_url']
+
+
+admin.site.register(URLRedirect,URLRedirectAdmin)
